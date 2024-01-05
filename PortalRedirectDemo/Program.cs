2f2385bcff219c224e443b06448fb24bb1f7483b
queueslikely <59344038+queueslikely@users.noreply.github.com>
2024-01-05 21:33:06 +0000

    Remove Swagger (can add it back later if needed)

--- PortalRedirectDemo/Program.cs
+++ PortalRedirectDemo/Program.cs
@@ -9,11 +9,7 @@ namespace PortalRedirectDemo
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
                 .AddNegotiate();
@@ -26,17 +22,9 @@ namespace PortalRedirectDemo
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
