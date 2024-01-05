e4ff77ace8fea6af202e98b72401941279ea1abe
queueslikely <59344038+queueslikely@users.noreply.github.com>
2024-01-05 19:19:53 +0000

    Create PortalRedirectController

---
+++ PortalRedirectDemo/Controllers/PortalRedirectController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalRedirectDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PortalRedirectController : ControllerBase
+    {
+        private IConfiguration configuration;
+
+        public PortalRedirectController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Student
+            if (User.IsInRole(configuration["Redirects:Student:GroupName"]))
+            {
+                return Redirect(configuration["Redirects:Student:RedirectUrl"]);
+            }
+
+            // Staff
+            if (User.IsInRole(configuration["Redirects:Staff:GroupName"]))
+            {
+                return Redirect(configuration["Redirects:Staff:RedirectUrl"]);
+            }
+
+            // Adult Ed
+            if (User.IsInRole(configuration["Redirects:AdultEd:GroupName"]))
+            {
+                return Redirect(configuration["Redirects:AdultEd:RedirectUrl"]);
+            }
+
+            // The user isn't in any of the above groups, so redirect them elsewhere
+            return Redirect(configuration["DefaultRedirectUrl"]);
+        }
+    }
+}
